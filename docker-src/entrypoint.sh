4c35c15e2286f8b1abaafc1706a6b21ab4b63c4c
Josh Pattman <Josh.pattman@gmail.com>
2023-02-07 09:25:52 +0000

    Initial files

---
+++ docker-src/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+PASSHASH=$(python3 -c "from notebook.auth import passwd; print(passwd('$PASS', 'sha1'))")
+
+echo "Notebook Password: '$PASS'"
+
+# Disables tensorflow whining about numa nodes
+for a in /sys/bus/pci/devices/*; do echo 0 | tee -a $a/numa_node; done
+
+# Continue execution
+PASSHASH=$PASSHASH exec "$@"
